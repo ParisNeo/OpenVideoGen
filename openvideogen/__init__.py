a63195f374bb7deae93d8138dc61da57561642aa
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-04-06 16:59:19 +0200

    Update __init__.py

--- openvideogen/__init__.py
+++ openvideogen/__init__.py
@@ -1,1 +1,27 @@
 
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2024 ParisNeo
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Open Source Video Generation API"""
+
+__author__ = "ParisNeo"
+__copyright__ = "Copyright 2024, ParisNeo"
+__credits__ = ["ParisNeo"]
+__license__ = "Apache 2.0"
+__version__ = "0.2.0" # <-- ADD THIS LINE (or your current version)
+__maintainer__ = "ParisNeo"
+__email__ = "parisneo_ai@gmail.com"
+__status__ = "Development" # Or "Production", "Alpha", etc.
