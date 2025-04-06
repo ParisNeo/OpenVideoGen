80ad48d9c7e647b72cf97ccb863d576db8c1125a
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-04-06 16:59:30 +0200

    Update __init__.py

--- openvideogen/__init__.py
+++ openvideogen/__init__.py
@@ -1,7 +1,7 @@
 
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2024 ParisNeo
+# Copyright (C) 2025 ParisNeo
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
